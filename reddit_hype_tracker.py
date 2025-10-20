7d20a3abac9590f909177feafb3cfeba8e8c3f0a
winko-coder <winko@gmx.at>
2025-10-20 07:13:25 +0200

    Update reddit_hype_tracker.py

--- reddit_hype_tracker.py
+++ reddit_hype_tracker.py
@@ -104,16 +104,19 @@ print(f"✅ CSV aktualisiert ({len(df_new)} neue Einträge).")
 # === Telegram-Zusammenfassung ===
 top_message = "<b>📊 Aktuelle Reddit-Trends</b>\n\n"
 top_message_2 = "\n<b>Aktuell interessante Sentiments</b>:\n"
+send_tm2 = false;
 for _, row in df_new.head(TOP_N).iterrows():
     line = f"• <b>{row['Ticker']}</b>: {row['Mentions']} Erwähnungen, Sentiment {row['Sentiment']:+.2f}, Kurs {row['CurrentPrice']}\n"
     if(row['Sentiment'] > 0.1 or row['Sentiment'] < -0.1):
         line2 = f"• <b>{row['Ticker']}</b>: {row['Mentions']} Erwähnungen, Sentiment {row['Sentiment']:+.2f}, Kurs {row['CurrentPrice']}\n"
         top_message_2 += line2
+        send_tm2 = true;
     top_message += line
 
 top_message += f"\n⏰ {datetime.now().strftime('%Y-%m-%d %H:%M')}"
-send_telegram_message(top_message)
-send_telegram_message(top_message_2)
+#send_telegram_message(top_message)
+if(send_tm2):
+    send_telegram_message(top_message_2)
 
 print("📨 Telegram-Update gesendet!")
 
